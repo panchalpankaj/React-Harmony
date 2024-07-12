f2526fbfccecb3d0aae086cb169e51ad9342101b
Panchal Pankaj <panchalpankaj89658@gmail.com>
2024-07-12 11:28:47 +0530

    add react bootstrap

--- harmoni/src/component/Navbar.jsx
+++ harmoni/src/component/Navbar.jsx
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "../Css/Navbar.css";
 import { Link, useNavigate } from "react-router-dom";
-import { faBars, faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCaretDown, faDroplet, faHamburger } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+
 
 export default function Navbar() {
   const [menuVisible, setMenuVisible] = useState(false);
   const [userData, setuserData] = useState({});
 
-  const navigate = useNavigate("");
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
 
-  // const handleNavigation = (path) => {
-  //   setMenuVisible(false);
-  //   navigate(path);
-  // };
+ 
 
   const tokan = sessionStorage.getItem(`accessToken`);
 
@@ -40,6 +38,7 @@ export default function Navbar() {
 
   return (
     <>
+    
       <div className="navbar">
         <div className="logo">
           <img src="/img/logo.png" alt="Logo" className=" mr-5 h-12 w-60 " />
@@ -70,7 +69,10 @@ export default function Navbar() {
               src={userData.avatar}
               className="size-10 rounded-full mr-2"
             ></img>
+            <div>
             {userData.fullName}
+           
+            </div>
           </div>
         ) : (
           <div className="asdfg">
