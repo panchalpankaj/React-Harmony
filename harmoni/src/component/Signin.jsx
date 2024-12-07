93483a8d3497380188a604da26f873681d791889
Panchal Pankaj <panchalpankaj89658@gmail.com>
2024-12-07 17:06:14 +0530

    after learning git all file keep as etis

--- harmoni/src/component/Signin.jsx
+++ harmoni/src/component/Signin.jsx
@@ -7,7 +7,6 @@ import { faLock } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-// check some code 
 
 export default function Signin() {
   
